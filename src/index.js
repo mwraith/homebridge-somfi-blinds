f9457c45820bff04d7b6452a161d3b71a11f5ecb
Mark Wraith <mark.wraith@gmail.com>
2025-08-22 23:43:11 +0100

    Resolving naming issue

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import { SomfyRtsPlatform } from './SomfyRtsPlatform.js';
 
 module.exports = (api) => {
 	// Old approach for registering accessories directly
-	api.registerAccessory('Somfy Blinds', SomfyRtsRemoteAccessory);
+	api.registerAccessory('homebridge-somfi-blinds', 'Somfy Blinds', SomfyRtsRemoteAccessory);
 
 	// New approach for registering platforms (supporting child bridges)
-    api.registerPlatform('Somfy Blinds', 'SomfyRtsPlatform', SomfyRtsPlatform);  
+    api.registerPlatform('homebridge-somfi-blinds', 'Somfy Blinds Platform', SomfyRtsPlatform);  
 };
   
